96b9a51ee077eb52bcc57548ffb2aa60c32058da
xiaoheng0800 <xiaoheng0800@163.com>
2022-05-08 10:15:11 +0800

    初次提交

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author gaoh
+ * @desc 1111
+ * @date 2022年05月08日 上午 10:13
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.print("qwerttreqwertyuioiuytrewertyu");
+    }
+}
